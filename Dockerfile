cf741e15535fd1553d2e1a3cef5d62769f0feb60
Sanket Patil <sp1524299@gmail.com>
2023-12-20 10:43:50 +0530

    files added

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+# Use the specified .NET SDK image
+FROM public.ecr.aws/sam/build-dotnet7:latest-arm64
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy everything from the src directory to /app in the container
+COPY . .
+
+# Publish the application to a folder with the specified runtime and configuration
+RUN dotnet publish -c Release -r linux-arm64 --self-contained true -o out
---
+++ Functions.cs
@@ -0,0 +1,116 @@
+using Amazon.Lambda.Core;
+using Amazon.Lambda.RuntimeSupport;
+using Amazon.Lambda.Serialization.SystemTextJson;
+using Amazon.Lambda.APIGatewayEvents;
+using System.Text.Json.Serialization;
+using System.Net;
+
+namespace AWSServerlessNativeAOT;
+
+public class Functions
+{
+    /// <summary>
+    /// The main entry point for the Lambda function. The main function is called once during the Lambda init phase. It
+    /// initializes the .NET Lambda runtime client passing in the function handler to invoke for each Lambda event and
+    /// the JSON serializer to use for converting Lambda JSON format to the .NET types. 
+    /// </summary>
+    private static async Task Main()
+    {
+        // The "_HANDLER" environment variable is set by the Lambda runtime to the configured value for the function
+        // handler property.
+        var configuredFunctionHandler = Environment.GetEnvironmentVariable("_HANDLER");
+
+        // Native AOT Lambda functions are deployed as a native executable using the provided.al2 Lambda runtime.
+        // The provided.al2 runtime ignores the function handler field and always looks for an executable called "bootstrap".
+        //
+        // As a convention this project template checks the value for the function handler field set in the
+        // serverless template to determine which method should be registered with the Lambda runtime client
+        // to respond to incoming Lambda events. This allows multiple Lambda functions be defined in the
+        // same .NET project using the provided.al2 runtime.
+        Func<APIGatewayHttpApiV2ProxyRequest, ILambdaContext, APIGatewayHttpApiV2ProxyResponse> handler = configuredFunctionHandler switch
+        {
+            nameof(GetFunctionHandler) => new Functions().GetFunctionHandler,
+            _ => throw new Exception($"Unknown method to call for handler value {configuredFunctionHandler}")
+        };
+
+        await LambdaBootstrapBuilder.Create(handler, new SourceGeneratorLambdaJsonSerializer<LambdaFunctionJsonSerializerContext>())
+            .Build()
+            .RunAsync();
+    }
+
+    /// <summary>
+    /// A Lambda function to respond to HTTP Get methods from API Gateway.
+    ///
+    /// To use this handler to respond to an AWS event, reference the appropriate package from 
+    /// https://github.com/aws/aws-lambda-dotnet#events
+    /// and change the input parameter to the desired event type. When the event type
+    /// is changed, the handler type registered in the main method needs to be updated and the LambdaFunctionJsonSerializerContext 
+    /// defined below will need the JsonSerializable updated. If the return type and event type are different then the 
+    /// LambdaFunctionJsonSerializerContext must have two JsonSerializable attributes, one for each type.
+    ///
+    // When using Native AOT extra testing with the deployed Lambda functions is required to ensure
+    // the libraries used in the Lambda function work correctly with Native AOT. If a runtime 
+    // error occurs about missing types or methods the most likely solution will be to remove references to trim-unsafe 
+    // code or configure trimming options. This sample defaults to partial TrimMode because currently the AWS 
+    // SDK for .NET does not support trimming. This will result in a larger executable size, and still does not 
+    // guarantee runtime trimming errors won't be hit. 
+    /// </summary>
+    /// <param name="request"></param>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public APIGatewayHttpApiV2ProxyResponse GetFunctionHandler(APIGatewayHttpApiV2ProxyRequest request, ILambdaContext context)
+    {
+        context.Logger.LogInformation("Get Request\n");
+      
+        var queryString = request.QueryStringParameters != null
+            ? string.Join("&", request.QueryStringParameters.Select(kv => $"{kv.Key}={kv.Value}"))
+            : "";
+        var architecture = System.Runtime.InteropServices.RuntimeInformation.ProcessArchitecture;
+        var dotnetVersion = Environment.Version.ToString();
+
+        var response = new APIGatewayHttpApiV2ProxyResponse
+        {
+            StatusCode = (int)HttpStatusCode.OK,
+            Body = $"Hello World !!! Query String: {queryString} version : {dotnetVersion} , architecture = {architecture}",
+            Headers = new Dictionary<string, string> { { "Content-Type", "text/plain" } }
+        };
+
+        return response;
+    }
+
+    /// <summary>
+    /// A Lambda function to respond to HTTP PUT methods from API Gateway
+    ///
+    /// To use this handler to respond to an AWS event, reference the appropriate package from 
+    /// https://github.com/aws/aws-lambda-dotnet#events
+    /// and change the input parameter to the desired event type. When the event type
+    /// is changed the handler type registered in the main needs to be updated and the LambdaFunctionJsonSerializerContext 
+    /// defined below will need the JsonSerializable updated. If the return type and event type are different then the 
+    /// LambdaFunctionJsonSerializerContext must have two JsonSerializable attributes, one for each type.
+    ///
+    // When using Native AOT extra testing with the deployed Lambda functions is required to ensure
+    // the libraries used in the Lambda function work correctly with Native AOT. If a runtime 
+    // error occurs about missing types or methods the most likely solution will be to remove references to trim-unsafe 
+    // code or configure trimming options. This sample defaults to partial TrimMode because currently the AWS 
+    // SDK for .NET does not support trimming. This will result in a larger executable size, and still does not 
+    // guarantee runtime trimming errors won't be hit. 
+    /// </summary>
+    /// <param name="request"></param>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    
+}
+
+/// <summary>
+/// This class is used to register the input event and return type for the FunctionHandler method with the System.Text.Json source generator.
+/// There must be a JsonSerializable attribute for each type used as the input and return type or a runtime error will occur 
+/// from the JSON serializer unable to find the serialization information for unknown types.
+/// </summary>
+[JsonSerializable(typeof(APIGatewayHttpApiV2ProxyRequest))]
+[JsonSerializable(typeof(APIGatewayHttpApiV2ProxyResponse))]
+public partial class LambdaFunctionJsonSerializerContext : JsonSerializerContext
+{
+    // By using this partial class derived from JsonSerializerContext, we can generate reflection free JSON Serializer code at compile time
+    // which can deserialize our class and properties. However, we must attribute this class to tell it what types to generate serialization code for.
+    // See https://docs.microsoft.com/en-us/dotnet/standard/serialization/system-text-json-source-generation
+}
